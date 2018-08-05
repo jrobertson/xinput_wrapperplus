ad29264770a44013a195e5456e79b8d2bf978aef
Your Name <you@example.com>
2018-08-05 15:32:56 +0100

    1st commit

---
+++ lib/xinput_wrapperplus.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+# file: xinput_wrapperplus.rb
+
+
+require 'sps-pub'
+require 'secret_knock'
+require 'xinput_wrapper'
+
+class XInputWrapperPlus < XInputWrapper
+
+  def initialize(device: '3', host: 'sps', port: '59000', 
+                 topic: 'input/keyboard', verbose: true, lookup: {}, 
+                 debug: false)
+
+    super(device: device, verbose: verbose, lookup: lookup, 
+          debug: debug)
+    @topic = topic
+    @sps = SPSPub.new host: host, port: port
+    @sk = SecretKnock.new short_delay: 0.25, long_delay: 0.5, 
+                              external: self, verbose: verbose, debug: debug
+    @sk.detect timeout: 0.7
+  end
+
+
+  def knock()
+    puts 'knock' if @verbose
+  end
+
+  def message(msg)
+    
+    puts ':: ' + msg.inspect if @verbose        
+    
+    return if msg.strip.empty?
+    
+    @sps.notice "%s: %s" % [@topic, msg]
+    
+  end  
+  
+  protected
+  
+  def on_control_key()
+    puts 'inside on_control_key' if @debug
+    @sk.knock
+  end
+  
+  def on_key_press(keycode)
+    puts 'inside on_key_press' if @debug
+    @sk.reset if @lookup[keycode] != :control
+  end
+  
+  def on_super_key()
+    message 'super key pressed' 
+  end
+end
+
